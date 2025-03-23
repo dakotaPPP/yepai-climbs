9a4d2e9b24e92c296e466fa7d46ae85a9dbe3388
dakotaPPP <dakota.pound04@gmail.com>
2025-03-22 20:28:41 -0500

    proper import statements now

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@
 // The base URL for the API
 // In production, this will be the Heroku domain
 // In development, this will be localhost
-export const API_URL = process.env.VITE_API_URL || 'http://localhost:8000';
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
 
 // Other configuration settings can be added here
 export const config = {
